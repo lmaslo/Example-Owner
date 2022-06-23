3b544775d70451fee80d32759668c88b12c4db74
Lena Masloboishchikova <perepechkinaEV@yandex.ru>
2022-06-23 10:20:54 +0300

    add configs

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -1,12 +1,30 @@
 package config;
 
+import java.util.Objects;
+
 public class WebDriverConfig {
 
-    public Browser getBrowser(){
-        return Browser.FIREFOX;
+    public Browser getBrowser() {
+        //Зачитываем данные из командной строки
+        String browser = System.getProperty("browser");
+        //Обрабатываем дефолтное значение
+        if (Objects.isNull(browser)) {
+            browser = "CHROME";
+        }
+        //Конвертируем в возращаемый тип
+        return Browser.valueOf(browser);
     }
 
-    public String getBaseUrl (){
-        return "https://github.com/";
+    public String getBaseUrl() {
+        //Зачитываем данные из командной строки
+        String baseUrl = System.getProperty("baseUrl");
+        //Обрабатываем дефолтное значение
+        if (Objects.isNull(baseUrl)) {
+            baseUrl = "https://github.com/";
+        }
+        //Конвертируем в возращаемый тип
+        return baseUrl;
     }
+
+
 }
